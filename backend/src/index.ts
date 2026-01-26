8904959930662b447c695a97800edc32032f1a87
omakhovskyi <leha1020947@gmail.com>
2026-01-26 10:51:14 +0100

    Init

---
+++ backend/src/index.ts
@@ -0,0 +1,9 @@
+import { createServer } from "./server";
+
+const PORT = process.env.PORT || 3000;
+
+const app = createServer();
+
+app.listen(PORT, () => {
+  console.log(`API running on http://localhost:${PORT}`);
+});
